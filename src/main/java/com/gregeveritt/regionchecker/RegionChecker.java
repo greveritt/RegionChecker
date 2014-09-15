deb7c12a5b560d4891d5f54d031c07cbda84325e
Gregory R. Everitt <me@gregeveritt.com>
2014-09-15 17:57:47 -0400

    Added Apache boilerplate disclaimer to class file

--- src/main/java/com/gregeveritt/regionchecker/RegionChecker.java
+++ src/main/java/com/gregeveritt/regionchecker/RegionChecker.java
@@ -2,6 +2,21 @@
  * The following has been ported to Java from C# and modified to work with the LatLng type
  * privided by Google Maps. Original code from Matt R on Stack Overflow
  * http://stackoverflow.com/questions/18486284/android-geofencing-polygon
+ *
+ * Copyright 2014 Gregory R. Everitt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * @author Gregory R. Everitt, Matthew Robbins
  * @version 0.5
  */
